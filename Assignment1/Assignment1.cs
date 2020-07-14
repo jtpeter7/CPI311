e75d0b5004cb8bcbbf0b4ed70dedd839727ede9f
jtpeter7 <55717969+jtpeter7@users.noreply.github.com>
2020-07-14 13:45:34 -0700

    Add files via upload

---
+++ Assignment1/Assignment1.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+using CPI311.GameEngine;
+
+namespace Assignment1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Assignment1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D Explorer;
+        Random random = new Random(160);
+        AnimatedSprite AnimateTheGuy;
+        //Sprite theSpriteGuy;
+        ProgressBar firstBar;
+        ProgressBar secondBar;
+        Sprite bonus;
+
+        public Assignment1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            Explorer = Content.Load<Texture2D>("explorer");
+            bonus = Content.Load<Sprite>("Bonus");
+            AnimateTheGuy = new AnimatedSprite(Explorer, 1);
+
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            InputManager.Update();
+            Time.Update(gameTime);
+
+            if (InputManager.IsKeyDown(Keys.W)) {
+                Explorer.Position += 5;
+            }
+            if (InputManager.IsKeyDown(Keys.S)) {
+
+            }
+            if (InputManager.IsKeyDown(Keys.A)) {
+
+            }
+            if (InputManager.IsKeyDown(Keys.D)) {
+
+            }
+
+            //Update animation right here
+
+            //if ((AnimateTheGuy.Position – bonus.Position).Length() < 32){
+            //    firstBar.Value += 10;
+            //    bonus.Position = new Vector2(random.Next(-160,160), random.Next(-160, 160));
+            //}
+
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            AnimateTheGuy.Draw(spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Assignment2/Assignment2.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using CPI311.GameEngine;
+
+namespace Assignment2
+{
+    public class Assignment2 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        float solR = 5;
+        float mercuryR = 2;
+        float earthR = 3;
+        float lunaR = 1;
+        Model solarSystem;
+        Model sol;
+        Model mercury;
+        Model earth;
+        Model luna;
+        Transform solarSystemTransform;
+        Transform solTransform;
+        Transform mercuryTransform;
+        Transform earthTransform;
+        Transform lunaTransform;
+        Transform cameraTransform;
+        Camera camera;
+
+        public Assignment2()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            Time.Initialize();
+            InputManager.Initialize();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            solarSystem = Content.Load<Model>("solarSystem");
+            sol = Content.Load<Model>("sol");
+            mercury = Content.Load<Model>("mercury");
+            earth = Content.Load<Model>("earth");
+            luna = Content.Load<Model>("luna");
+            solarSystemTransform = new Transform();
+            solTransform = new Transform(); 
+            mercuryTransform = new Transform(); mercuryTransform.LocalPosition += Vector3.Right*50;
+            earthTransform = new Transform(); earthTransform.LocalPosition += Vector3.Left*75;
+            lunaTransform = new Transform(); lunaTransform.LocalPosition += Vector3.Up*25;
+            cameraTransform = new Transform();
+            cameraTransform.LocalPosition = Vector3.Backward * 100;
+            camera = new Camera();
+            camera.Transform = cameraTransform;
+            mercuryTransform.Parent = solTransform;
+            earthTransform.Parent = solTransform;
+            lunaTransform.Parent = earthTransform;
+        }
+
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            Time.Update(gameTime);
+            InputManager.Update();
+
+            solTransform.Rotate(Vector3.Forward, Time.ElapsedGameTime);
+            earthTransform.Rotate(Vector3.Backward, Time.ElapsedGameTime*2);
+
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            solarSystem.Draw(solarSystemTransform.World, camera.View, camera.Projection);
+            sol.Draw(solTransform.World, camera.View, camera.Projection);
+            mercury.Draw(mercuryTransform.World, camera.View, camera.Projection);
+            earth.Draw(earthTransform.World, camera.View, camera.Projection);
+            luna.Draw(lunaTransform.World, camera.View, camera.Projection);
+
+            base.Draw(gameTime);
+        }
+    }
+}
