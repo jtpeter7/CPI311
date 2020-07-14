77b1304730889ba766d2a3f21141e9be460a76f0
jtpeter7 <55717969+jtpeter7@users.noreply.github.com>
2020-07-14 13:28:15 -0700

    Add files via upload

---
+++ Lab05/Lab05.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using CPI311.GameEngine;
+
+namespace Lab05
+{
+    public class Lab05 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Model model;
+        Effect effect;
+        Texture2D texture;
+        Transform parentTransform;
+        Transform cameraTransform;
+        Camera camera;
+
+        public Lab05()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            model = Content.Load<Model>("Torus");
+            effect = Content.Load<Effect>("SimpleShading");
+            texture = Content.Load<Texture2D>("Square");
+
+            parentTransform = new Transform();
+            cameraTransform = new Transform();
+            cameraTransform.LocalPosition = Vector3.Backward * 3;
+            camera = new Camera();
+            camera.Transform = cameraTransform;
+        }
+
+        protected override void UnloadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            time.Update(gameTime);
+            InputManager.Update();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Matrix view = camera.View;
+            Matrix projection = camera.Projection;
+
+            effect.CurrentTechnique = effect.Techniques[0];
+            effect.Parameters["World"].SetValue(parentTransform.World);
+            effect.Parameters["View"].SetValue(view);
+            effect.Parameters["Projection"].SetValue(projection);
+            effect.Parameters["LightPosition"].SetValue(Vector3.Backward * 10 + Vector3.Right * 5);
+
+            effect.Parameters["CameraPosition"].SetValue(cameraTransform.Position);
+            effect.Parameters["Shininess"].SetValue(20f);
+            effect.Parameters["AmbientColor"].SetValue(new Vector3(0.2f, 0.2f, 0.2f));
+            effect.Parameters["DiffuseColor"].SetValue(new Vector3(0.5f, 0, 0));
+            effect.Parameters["SpecularColor"].SetValue(new Vector3(0, 0, 0.5f));
+            effect.Parameters["DiffuseTexture"].SetValue(texture);
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                foreach (ModelMesh mesh in model.Meshes)
+                    foreach (ModelMeshPart part in mesh.MeshParts)
+                    {
+                        GraphicsDevice.SetVertexBuffer(part.VertexBuffer);
+                        GraphicsDevice.Indices = part.IndexBuffer;
+                        GraphicsDevice.DrawIndexedPrimitives(
+                            PrimitiveType.TriangleList, part.VertexOffset, 0,
+                            part.NumVertices, part.StartIndex, part.PrimitiveCount);
+                    }
+            }
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Lab06/Lab06.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using CPI311.GameEngine;
+using System;
+using System.Collections.Generic;
+
+namespace Lab06
+{
+    public class Lab06 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Model model;
+
+        BoxCollider boxCollider;
+        SphereCollider sphere1, sphere2;
+        Random random;
+        List<Rigidbody> rigidbodies;
+        List<Collider> colliders;
+        List<Transform> transforms;
+
+        Camera camera;
+        Transform cameraTransform;
+
+        int numberCollisions;
+
+        public Lab06()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            random = new Random();
+            transforms = new List<Transform>();
+            rigidbodies = new List<Rigidbody>();
+            colliders = new List<Collider>();
+            boxCollider = new BoxCollider();
+            boxCollider.Size = 10;
+
+            for (int i = 0; i < 2; i++)
+            {
+                Transform transform = new Transform();
+                transform.LocalPosition += Vector3.Right * 2 * i; //avoid overlapping each sphere **CHANGE LATER**
+                Rigidbody rigidbody = new Rigidbody();
+                rigidbody.Transform = transform;
+                rigidbody.Mass = 1;
+
+                Vector3 direction = new Vector3(
+                  (float)random.NextDouble(), (float)random.NextDouble(),
+                  (float)random.NextDouble());
+                direction.Normalize();
+                rigidbody.Velocity =
+                   direction * ((float)random.NextDouble() * 5 + 5);
+                SphereCollider sphereCollider = new SphereCollider();
+                sphereCollider.Radius = 2.5f * transform.LocalScale.Y;
+                sphereCollider.Transform = transform;
+
+                transforms.Add(transform);
+                colliders.Add(sphereCollider);
+                rigidbodies.Add(rigidbody);
+            }
+
+
+                base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            model = Content.Load<Model>("Sphere");
+            cameraTransform = new Transform();
+            cameraTransform.LocalPosition = Vector3.Backward * 20;
+            camera = new Camera();
+            camera.Transform = cameraTransform;
+        }
+
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+
+            foreach (Rigidbody rigidbody in rigidbodies) rigidbody.Update();
+            Vector3 normal; // it is updated if a collision happens
+            for (int i = 0; i < transforms.Count; i++)
+            {
+                if (boxCollider.Collides(colliders[i], out normal))
+                {
+                    numberCollisions++;
+                    if (Vector3.Dot(normal, rigidbodies[i].Velocity) < 0)
+                        rigidbodies[i].Impulse +=
+                           Vector3.Dot(normal, rigidbodies[i].Velocity) * -2 * normal;
+                }
+                for (int j = i + 1; j < transforms.Count; j++)
+                {
+                    if (colliders[i].Collides(colliders[j], out normal))
+                        numberCollisions++;
+
+                    Vector3 velocityNormal = Vector3.Dot(normal,
+                        rigidbodies[i].Velocity - rigidbodies[j].Velocity) * -2
+                           * normal * rigidbodies[i].Mass * rigidbodies[j].Mass;
+                    rigidbodies[i].Impulse += velocityNormal / 2;
+                    rigidbodies[j].Impulse += -velocityNormal / 2;
+                }
+            }
+
+            InputManager.Update();
+            time.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            foreach (Transform transform in transforms)
+                model.Draw(transform.World, camera.View, camera.Projection);
+
+            base.Draw(gameTime);
+        }
+    }
+}
