7970a582b8a7fdc89ad93f839779840ee56dd7f0
jtpeter7 <55717969+jtpeter7@users.noreply.github.com>
2020-07-14 13:41:03 -0700

    Add files via upload

---
+++ Lab09/Lab09.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using CPI311.GameEngine;
+using System.Collections.Generic;
+using System;
+
+namespace Lab09
+{
+    public class Lab09 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Model cube;
+        Model sphere;
+        AStarSearch search;
+        List<Vector3> path;
+        Model enemy;
+
+        TimeSpan totalTime;
+        int index = 0;
+
+        Random random = new Random();
+        Camera camera;
+        int size = 30; // 10x10 grid
+        Transform cameraTransform;
+
+        public Lab09()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            time.Initialize();
+            InputManager.Initialize();
+            ScreenManager.Initialize(graphics);
+
+            search = new AStarSearch(size, size); // size of grid 
+
+            foreach (AStarNode node in search.Nodes)
+                if (random.NextDouble() < 0.2)
+                    search.Nodes[random.Next(size), random.Next(size)].Passable = false;
+
+            search.Start = search.Nodes[0, 0];
+            search.Start.Passable = true;
+            search.End = search.Nodes[size - 1, size - 1];
+            search.End.Passable = true;
+
+            search.Search(); // A search is made here.
+
+            path = new List<Vector3>();
+            AStarNode current = search.End;
+            while (current != null)
+            {
+                path.Insert(0, current.Position);
+                current = current.Parent;
+            }
+
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            sphere = Content.Load<Model>("Sphere");
+            cube = Content.Load<Model>("Box2");
+            enemy = Content.Load<Model>("Torus");
+
+            camera = new Camera();
+            cameraTransform = new Transform();
+            cameraTransform.LocalPosition = new Vector3(15, 30, 15);
+            camera.Transform = cameraTransform;
+            camera.Transform.Rotate(Vector3.Right, -MathHelper.PiOver2);
+        }
+
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            time.Update(gameTime);
+            InputManager.Update();
+
+            if(InputManager.IsKeyPressed(Keys.Space))
+                {
+                    search = new AStarSearch(size, size);
+                foreach (AStarNode node in search.Nodes)
+                    if (random.NextDouble() < 0.2)
+                        search.Nodes[random.Next(size), random.Next(size)].Passable = false;
+
+                    search.Start = search.Nodes[random.Next(search.Cols), random.Next(search.Rows)];
+                    search.Start.Passable = true;
+                    search.End = search.Nodes[random.Next(search.Cols), random.Next(search.Rows)];
+                    search.End.Passable = true;
+
+                    search.Search(); 
+
+                    path = new List<Vector3>();
+                    AStarNode current = search.End;
+                    while (current != null)
+                    {
+                     path.Insert(0, current.Position);
+                        current = current.Parent;
+                    }
+
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            (cube.Meshes[0].Effects[0] as BasicEffect).DiffuseColor = Color.Red.ToVector3();
+
+            foreach (AStarNode node in search.Nodes)
+                if (!node.Passable)
+                    cube.Draw(Matrix.CreateScale(0.5f, 0.05f, 0.5f) *
+                       Matrix.CreateTranslation(node.Position), camera.View, camera.Projection);
+
+            foreach (Vector3 position in path)
+                sphere.Draw(Matrix.CreateScale(0.1f, 0.1f, 0.1f) *
+                     Matrix.CreateTranslation(position), camera.View, camera.Projection);
+
+            totalTime += gameTime.ElapsedGameTime;
+
+            if (totalTime > TimeSpan.FromSeconds(1))
+            {
+                totalTime = totalTime.Subtract(TimeSpan.FromSeconds(1));
+                index++;
+                index = Math.Min(search.Nodes.Length - 2, index);
+            }
+
+            Vector3 pos1 = path[index];
+            Vector3 pos2 = path[index+1];
+            Vector3 diff = pos2 - pos1;
+
+            (enemy.Meshes[0].Effects[0] as BasicEffect).DiffuseColor = Color.HotPink.ToVector3();
+            enemy.Draw(Matrix.CreateScale(0.2f, 0.2f, 0.2f) *
+                    Matrix.CreateTranslation(pos1 + diff), camera.View, camera.Projection);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Lab10/Lab10.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using CPI311.GameEngine;
+
+namespace Lab10
+{
+    public class Lab10 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        TerrainRenderer terrain;
+        Camera camera;
+        Effect effect;
+
+
+        public Lab10()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            //*** MonoGame3.6 *******************************
+            graphics.GraphicsProfile = GraphicsProfile.HiDef;
+            //***********************************************
+
+        }
+
+        protected override void Initialize()
+        {
+            time.Initialize();
+            InputManager.Initialize();
+            ScreenManager.Initialize(graphics);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            terrain = new TerrainRenderer(
+                Content.Load<Texture2D>("Heightmap"),
+                Vector2.One * 100, Vector2.One * 200);
+
+            terrain.NormalMap = Content.Load<Texture2D>("Normalmap");
+            terrain.Transform = new Transform();
+            terrain.Transform.LocalScale *= new Vector3(1, 5, 1);
+
+            effect = Content.Load<Effect>("TerrainShader");
+            effect.Parameters["AmbientColor"].SetValue(new Vector3(0.1f, 0.1f, 0.1f));
+            effect.Parameters["DiffuseColor"].SetValue(new Vector3(0.3f, 0.1f, 0.1f));
+            effect.Parameters["SpecularColor"].SetValue(new Vector3(0, 0, 0.2f));
+            effect.Parameters["Shininess"].SetValue(20f);
+
+            camera = new Camera();
+            camera.Transform = new Transform();
+            camera.Transform.LocalPosition = Vector3.Backward * 5 + Vector3.Right * 5 + Vector3.Up * 5;
+        }
+
+        protected override void UnloadContent()
+        {
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            time.Update(gameTime);
+            InputManager.Update();
+
+            if (InputManager.IsKeyDown(Keys.W)) 
+            {
+                camera.Transform.LocalPosition += camera.Transform.Forward * time.ElapsedGameTime;
+            }
+            if (InputManager.IsKeyDown(Keys.S)) 
+            {
+                camera.Transform.LocalPosition += camera.Transform.Backward * time.ElapsedGameTime;
+            }
+            if (InputManager.IsKeyDown(Keys.A)) 
+            {
+                camera.Transform.Rotate(Vector3.Up, time.ElapsedGameTime);
+            }
+            if (InputManager.IsKeyDown(Keys.D)) 
+            {
+                camera.Transform.Rotate(Vector3.Down, time.ElapsedGameTime);
+            }
+
+            camera.Transform.LocalPosition = new Vector3(camera.Transform.LocalPosition.X, terrain.GetAltitude(camera.Transform.LocalPosition), camera.Transform.LocalPosition.Z);
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            effect.Parameters["NormalMap"].SetValue(terrain.NormalMap);
+            effect.Parameters["World"].SetValue(terrain.Transform.World);
+            effect.Parameters["View"].SetValue(camera.View);
+            effect.Parameters["Projection"].SetValue(camera.Projection);
+            effect.Parameters["LightPosition"].SetValue(camera.Transform.Position + Vector3.Up * 10);
+            effect.Parameters["CameraPosition"].SetValue(camera.Transform.Position);
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                terrain.Draw();
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
